fb234a6bc9b833a783c2cd9c79091a65a69046b9
Mark Mnei <MarkMnei@gmail.com>
2016-05-20 22:04:05 -0400

    Updated files

--- demo/src/main/java/demo/DemoApplication.java
+++ demo/src/main/java/demo/DemoApplication.java
@@ -1,5 +1,6 @@
 package demo;
 
+import jdk.nashorn.internal.objects.annotations.Setter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -9,7 +10,11 @@ public class DemoApplication {
 
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(DemoApplication.class, args);
-        HelloWorld helloWorld = (HelloWorld) ctx.getBean("helloWorld");
-        helloWorld.sayHello();
+
+        InjectedByConstructorService constructorService = (InjectedByConstructorService) ctx.getBean("injectedByConstructorService");
+        constructorService.getMessage();
+
+        SetterBasedService setterBasedService = (SetterBasedService) ctx.getBean("setterBasedService");
+        setterBasedService.getMessage();
     }
 }
